8a6edd7d456bf6d0da78d917014037b2d837d6b5
Shriwalkade <shrivallabhwalkade@gmail.com>
2024-07-23 10:33:01 +0530

    main updated

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from input import test_number_to_pair,test_pair_to_number
+from print import print_output
+
+
 if __name__ == '__main__':
   test_number_to_pair(4, 'White', 'Brown')
   test_number_to_pair(5, 'White', 'Slate')
   test_pair_to_number('Black', 'Orange', 12)
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
-  print('Done :)')
+  print_output('Done :)')
+  
